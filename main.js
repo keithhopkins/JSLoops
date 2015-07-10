f9f93d5955d140e4d561f7dd7146f639224dd430
keithhopkins <knhopkins412@gmail.com>
2015-07-10 15:39:21 -0600

    complete

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("Problem 1");
+for (var i = 0; i <= 1000; i+=100)
+  console.log(i);
+console.log("Problem 2");
+for (var k = 1; k<=128; k*=2)
+  console.log(k);
+console.log("Problem 3");
+for (var j = 0; j<=10; j+=2)
+  console.log(j);
+console.log("Problem 4");
+for (var h = 3; h<=15; h+=3)
+  console.log(h);
+console.log("Problem 5");
+for (var g = 9; g>=0; g--)
+  console.log(g);
+console.log("Problem 6");
+for (var f = 1; f<4; f++)
+  for (var u = 0; u<3; u++)
+    console.log(f);
+console.log("Problem 7");
+for(var p = 0; p<4; p++)
+  for(var t = 0; t<5; t++)
+    console.log(t);
